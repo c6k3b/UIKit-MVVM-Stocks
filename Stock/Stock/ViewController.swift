348fa06366eb31aeb4f9562a3806176afae04b6a
AA <c6k3b@icloud.com>
2022-11-30 18:44:35 +0400

    initial

---
+++ Stock/Stock/ViewController.swift
@@ -0,0 +1,15 @@
+//  ViewController.swift
+//  Created by aa on 11/30/22.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
