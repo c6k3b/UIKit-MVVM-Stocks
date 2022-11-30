a70263854d04f1ce9d83e1d8e40870695e548219
AA <c6k3b@icloud.com>
2022-11-30 20:33:48 +0400

    search controller

--- Stock/Stock/Controllers/WatchListViewController.swift
+++ Stock/Stock/Controllers/WatchListViewController.swift
@@ -7,5 +7,24 @@ class WatchListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setUpSearchController()
+    }
+
+    private func setUpSearchController() {
+        let resultsVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: resultsVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
+}
+
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let resultsVC = searchController.searchResultsController as? SearchResultsViewController,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty
+        else { return }
+
+        print(query)
     }
 }
